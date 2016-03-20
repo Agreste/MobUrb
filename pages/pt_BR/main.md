ca40e7cf9810fdc244706c7016448fb8c558f838
Edgar Zanella Alvarenga <e@vaz.io>
2016-03-20 22:09:36 +0100

    Player! Ainda sem Borbagato

--- pages/pt_BR/main.md
+++ pages/pt_BR/main.md
@@ -1,7 +1,4 @@
-teste
 
-Aqui vai o video (sem caixa, mas com uma latgura semelhante à desta caixa)
-
-Precisamos fazer a menubar encima só aparecer quando põe o mouse encima, pelo menos nessa página. Depois arrumo os links dela. Não consigo fazer a div ali ter a mesma altura que a imagem.
-
-Sobrou uma barra preta imensa embaixo. saca o que é?
+<video id="videoPlaying" controls style="background-color: 'black';" preload="auto" poster="/static/img/transparent.png">
+<source src="" type="video/mp4">
+</video>
---
+++ static/javascripts/video_player.js
@@ -0,0 +1,20 @@
+startVideos = function(videoId, playlist) {
+   var video_idx = 0;
+   var videoPlaying = document.getElementById(videoId);
+   var videoSource = videoPlaying.children[0];
+
+   videoPlaying.addEventListener('ended', changeVideo, false);
+
+   function changeVideo(event) {
+       videoSource.setAttribute('src', playlist[video_idx]);
+       videoPlaying.load();
+       videoPlaying.play();
+       videoPlaying.style.width = '100%';
+       video_idx += 1;
+       if (video_idx == playlist.length) {
+           video_idx = 0;
+       }
+   }
+
+   changeVideo();
+}
