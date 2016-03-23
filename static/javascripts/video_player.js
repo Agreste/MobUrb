4a147ecea0d1d6d3a4673b8485d99de78eee4163
Edgar Zanella Alvarenga <e@vaz.io>
2016-03-23 13:28:11 +0100

    Adding subtitles

--- static/javascripts/video_player.js
+++ static/javascripts/video_player.js
@@ -1,21 +1,41 @@
 startVideos = function(videoId, playlist) {
-   var video_idx = 0;
-   var videoPlaying = document.getElementById(videoId);
-   var videoSource = videoPlaying.children[0];
+    var video_idx = 0;
+    var videoPlaying = document.getElementById(videoId);
+    var videoSource = videoPlaying.children[0];
 
-   videoPlaying.addEventListener('ended', changeVideo, false);
+    videoPlaying.addEventListener('ended', changeVideo, false);
+    console.log(playlist);
 
-   function changeVideo(event) {
-       videoSource.setAttribute('src', playlist[video_idx]);
-       videoPlaying.load();
-       console.log(event);
-       if (typeof(event) !== 'undefined')
-           videoPlaying.play();
-       video_idx += 1;
-       if (video_idx == playlist.length) {
-           video_idx = 0;
-       }
-   }
+    function changeVideo(event) {
+        videoSource.setAttribute('src', playlist[video_idx][0]);
 
-   changeVideo();
+        if (playlist[video_idx][1] !== 'NOSUB') {
+            console.log(playlist[video_idx]);
+            videoPlaying.addEventListener("loadedmetadata", function() {
+                for (var i = 1; i < videoPlaying.children.length; ++i) {
+                    videoPlaying.removeChild(videoPlaying.children[i]);
+                }
+                track = document.createElement("track");
+                track.kind = "captions";
+                track.label = "Português";
+                track.srclang = "pt";
+                track.src = "/static/subtitles/" + playlist[video_idx-1][1];
+                track.addEventListener("load", function() {
+                    this.mode = "showing";
+                    videoPlaying.textTracks[0].mode = "showing"; // thanks Firefox
+                });
+                this.appendChild(track);
+            });
+        }
+
+        videoPlaying.load();
+        if (typeof(event) !== 'undefined')
+            videoPlaying.play();
+        video_idx += 1;
+        if (video_idx == playlist.length) {
+            video_idx = 0;
+        }
+    }
+
+    changeVideo();
 }
